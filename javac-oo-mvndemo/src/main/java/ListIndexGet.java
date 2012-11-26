d9b3da3eae5ca22410c58afadbe2a49c94d14cd6
Artem Melentyev <amelentev@gmail.com>
2012-11-26 13:16:50 +0600

    mvndemo uses latest oo snapshot and compile symlinks to examples

--- javac-oo-mvndemo/src/main/java/ListIndexGet.java
+++ javac-oo-mvndemo/src/main/java/ListIndexGet.java
@@ -1,16 +1,1 @@
-import java.util.Arrays;
-import java.util.List;
-
-public class ListIndexGet {
-    public static boolean test() {
-        List<Integer> lst = Arrays.asList(1);
-        // without OO:
-        int resl1 = lst.get(0);
-        // with OO:
-        int resl2 = lst[0];
-        return resl2 == resl1;
-    }
-    public static void main(String[] args) {
-		System.out.println(ListIndexGet.test());
-	}
-}
+../../../../examples/ListIndexGet.java
--- javac-oo-mvndemo/src/main/java/MapIndex.java
+++ javac-oo-mvndemo/src/main/java/MapIndex.java
@@ -1,18 +1,1 @@
-import java.util.HashMap;
-
-public class MapIndex {
-    public static boolean test() {
-        HashMap<String, String> map = new HashMap<>();
-        // without OO:
-        map.put("qwe", "qwe");
-        String r1 = map.get("qwe");
-        // with OO:
-        map["qwe"] = "asd";
-        String r2 = map["qwe"];
-        
-        return "asd".equals(r2) && "qwe".equals(r1);
-    }
-    public static void main(String[] args) {
-		System.out.println(MapIndex.test());
-	}
-}
+../../../../examples/MapIndex.java
