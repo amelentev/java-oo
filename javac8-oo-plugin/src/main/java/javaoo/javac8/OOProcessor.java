2f37ece401eaa1a186f7096e79400bb6baaf79ac
Artem Melentyev <amelentev@gmail.com>
2014-02-03 01:51:48 +0300

    javac8: set source&target versions to 7. use nbjavac as compile dep instead of tools.jar

--- javac8-oo-plugin/src/main/java/javaoo/javac8/OOProcessor.java
+++ javac8-oo-plugin/src/main/java/javaoo/javac8/OOProcessor.java
@@ -12,10 +12,7 @@ package javaoo.javac8;
 import com.sun.source.util.TaskEvent;
 import com.sun.source.util.TaskListener;
 import com.sun.tools.javac.api.MultiTaskListener;
-import com.sun.tools.javac.comp.Attr;
-import com.sun.tools.javac.comp.MemberEnter;
-import com.sun.tools.javac.comp.Resolve;
-import com.sun.tools.javac.comp.TransTypes;
+import com.sun.tools.javac.comp.*;
 import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.util.Context;
@@ -40,7 +37,8 @@ public class OOProcessor extends AbstractProcessor {
         try {
             ClassLoader pclassloader = (ClassLoader) get(pe, "processorClassLoader");
             if (pclassloader != null && (!pclassloader.getClass().equals(ClassLoader.class)))
-                set(pe, "processorClassLoader", new ClassLoader(pclassloader) {});
+                // do not let compiler to close our classloader. we need it later.
+                set(pe, JavacProcessingEnvironment.class, "processorClassLoader", new ClassLoader(pclassloader) {});
             if (pclassloader == null) { // netbeans
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Injecting OO to netbeans");
                 patch(compiler, OOProcessor.class.getClassLoader());
@@ -50,9 +48,7 @@ public class OOProcessor extends AbstractProcessor {
             final MultiTaskListener taskListener = (MultiTaskListener) get(compiler, "taskListener");
             taskListener.add(new WaitAnalyzeTaskListener(compiler, pclassloader));
         } catch (Exception e) {
-            if (e instanceof RuntimeException)
-                throw (RuntimeException)e;
-            throw new RuntimeException(e);
+            sneakyThrow(e);
         }
     }
 
@@ -71,7 +67,7 @@ public class OOProcessor extends AbstractProcessor {
         }
         @Override
         public void started(TaskEvent e) {
-            if (e.getKind() == TaskEvent.Kind.ANALYZE && !done) {
+            if (!done && e.getKind() == TaskEvent.Kind.ANALYZE) {
                 patch(compiler, pclassloader);
                 done = true;
             }
@@ -80,50 +76,39 @@ public class OOProcessor extends AbstractProcessor {
         public void finished(TaskEvent e) {}
     }
 
-    static Object get(Object obj, String field) throws ReflectiveOperationException {
-        Field f = obj.getClass().getDeclaredField(field);
-        f.setAccessible(true);
-        return f.get(obj);
-    }
-    static void set(Object obj, String field, Object val) throws ReflectiveOperationException {
-        Field f = obj.getClass().getDeclaredField(field);
-        f.setAccessible(true);
-        f.set(obj, val);
-    }
-    static Object getInstance(Class<?> clas, Context context) throws ReflectiveOperationException {
-        return clas.getDeclaredMethod("instance", Context.class).invoke(null, context);
-    }
-
     static void patch(JavaCompiler compiler, ClassLoader pcl) {
         try {
             JavaCompiler delCompiler = (JavaCompiler) get(compiler, "delegateCompiler");
             if (delCompiler != null)
                 compiler = delCompiler; // javac has delegateCompiler. netbeans hasn't
             Context context = (Context) get(compiler, "context");
+            Attr attr = Attr.instance(context);
+            if (attr instanceof OOAttr)
+                return;
+            ClassLoader destcl = attr.getClass().getClassLoader();
 
             // hack: load classes to the same classloader so they will be able to use and override default accessor members
-            Class<?> attrClass = reloadClass("com.sun.tools.javac.comp.OOAttr", pcl, Attr.class.getClassLoader());
-            Class<?> resolveClass = reloadClass("com.sun.tools.javac.comp.OOResolve", pcl, Resolve.class.getClassLoader());
-            Class<?> transTypesClass = reloadClass("com.sun.tools.javac.comp.OOTransTypes", pcl, TransTypes.class.getClassLoader());
-            reloadClass("javaoo.OOMethods", pcl, TransTypes.class.getClassLoader());
-            reloadClass("javaoo.OOMethods$1", pcl, TransTypes.class.getClassLoader());
-            reloadClass("javaoo.OOMethods$2", pcl, TransTypes.class.getClassLoader());
+            Class<?> attrClass = reloadClass("com.sun.tools.javac.comp.OOAttr", pcl, destcl);
+            Class<?> resolveClass = reloadClass("com.sun.tools.javac.comp.OOResolve", pcl, destcl);
+            Class<?> transTypesClass = reloadClass("com.sun.tools.javac.comp.OOTransTypes", pcl, destcl);
+            reloadClass("javaoo.OOMethods", pcl, destcl);
+            reloadClass("javaoo.OOMethods$1", pcl, destcl);
+            reloadClass("javaoo.OOMethods$2", pcl, destcl);
 
             getInstance(resolveClass, context);
-            Object attr = getInstance(attrClass, context);
+            attr = (Attr) getInstance(attrClass, context);
             Object transTypes = getInstance(transTypesClass, context);
 
-            set(compiler, "attr", attr);
-            set(MemberEnter.instance(context), "attr", attr);
-            set(compiler, "transTypes", transTypes);
+            set(compiler, JavaCompiler.class, "attr", attr);
+            set(compiler, JavaCompiler.class, "transTypes", transTypes);
+            set(MemberEnter.instance(context), MemberEnter.class, "attr", attr);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            sneakyThrow(e);
         }
     }
-
     @SuppressWarnings("unchecked")
     /** add class claz to outClassLoader */
-    static <T> Class<T> reloadClass(String claz, ClassLoader incl, ClassLoader outcl) throws Exception {
+    static <T> Class<T> reloadClass(final String claz, ClassLoader incl, ClassLoader outcl) throws Exception {
         try { // already loaded?
             return (Class<T>) outcl.loadClass(claz);
         } catch (ClassNotFoundException e) {}
@@ -135,5 +120,26 @@ public class OOProcessor extends AbstractProcessor {
                 String.class, byte[].class, int.class, int.class });
         m.setAccessible(true);
         return (Class<T>) m.invoke(outcl, claz, bytes, 0, bytes.length);
+    }
+
+    // reflection stuff
+    static Object getInstance(Class<?> clas, Context context) throws ReflectiveOperationException {
+        return clas.getDeclaredMethod("instance", Context.class).invoke(null, context);
+    }
+    static Object get(Object obj, String field) throws ReflectiveOperationException {
+        Field f = obj.getClass().getDeclaredField(field);
+        f.setAccessible(true);
+        return f.get(obj);
+    }
+    static void set(Object obj, Class clas, String field, Object val) throws ReflectiveOperationException {
+        Field f = clas.getDeclaredField(field);
+        f.setAccessible(true);
+        f.set(obj, val);
+    }
+    public static void sneakyThrow(Throwable ex) {
+        OOProcessor.<RuntimeException>sneakyThrowInner(ex);
+    }
+    private static <T extends Throwable> T sneakyThrowInner(Throwable ex) throws T {
+        throw (T) ex;
     }
 }
