73dd698c77ebad4344400ef901d016a64b845ba4
Artem Melentyev <artem@melentyev.me>
2016-02-02 00:23:51 +0200

    assignment op overload in Cmp demo

--- javac-oo-mvndemo/src/Cmp.java
+++ javac-oo-mvndemo/src/Cmp.java
@@ -2,13 +2,15 @@ import java.math.BigDecimal;
 
 public class Cmp {
     public static boolean test() {
-        BigDecimal a = BigDecimal.ZERO,
-                b = BigDecimal.ONE,
-                c = BigDecimal.valueOf(1);
         // without OO:
-        boolean r1 = a.compareTo(b)<0 && c.compareTo(a)>0 && b.compareTo(c)>=0 && c.compareTo(b)>=0;
+        BigDecimal a = BigDecimal.valueOf(0),
         // with OO:
-        boolean r2 = a < b && c > a && b >= c && c >= b;
+                b = 1,
+                c = 2;
+        // without OO:
+        boolean r1 = a.compareTo(b)<0 && c.compareTo(a)>0 && (b.add(b)).compareTo(c)>=0 && c.compareTo(b.add(b))>=0;
+        // with OO:
+        boolean r2 = a < b && c > a && b+b >= c && c >= b+b;
         System.out.printf("(a < b && c > a && b >= c && c >= b) = %b\n", r2);
         System.out.printf("Here a,b,c's class is %s, and a=%s, b=%s, c=%s\n", a.getClass().getSimpleName(), a, b, c);
         return r1 && r2;
